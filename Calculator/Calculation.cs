6efec86e8892c7831ffe41034c59e015103cc547
Md Jahid Hasan <joyh304@gmail.com>
2021-04-24 18:03:24 +0600

    calculator using delegates

---
+++ Calculator/Calculation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculation
+    {
+        public int Addition(int x,int y)
+        {
+            int result = x + y;
+            Console.WriteLine("Addition:" + result);
+            return result;
+        }
+        public int multi(int x,int y)
+        {
+            int result = x * y;
+            Console.WriteLine("Multiplication:"+result);
+            return result;
+        }
+        public int Substract(int x, int y)
+        {
+            int result = x - y;
+            Console.WriteLine("Subtracttion:"+result);
+            return result;
+        }
+        public int Division(int x, int y)
+        {
+            int result = x / y;
+            Console.WriteLine("Division:"+result);
+            return result;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        public delegate int clac(int a, int b);
+        static void Main(string[] args)
+        {
+            Calculation cal = new Calculation();
+            clac cal1 = cal.Addition;
+            clac cal2 = cal.Division;
+            clac cal3 = cal.multi;
+            clac cal4 = cal.Substract;
+            clac calculate = cal1 + cal2 + cal3 + cal4;
+
+            runcalculation(calculate);
+            
+        }
+
+        private static int runcalculation(clac Clac)
+        {
+            int r = Clac.Invoke(40, 5);
+            return r;
+        }
+    }
+}
